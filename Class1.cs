7d17135faf0472aaacf15151cec725f984945a9a
DuradazhiVlad <dvlad1995p@gmail.com>
2022-12-16 19:15:20 +0200

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Extesion_Methods
+{
+    public static class ExtensionMethod
+    {
+        public static int ToInt(this bool a)
+        {
+            if (a == true) { return 1; }
+            else { return 0; }
+           
+        }
+
+        public static DateTime ToDate(this string a)
+        {
+            string[] s = a.Split(new char[] { ',', '.' },StringSplitOptions.RemoveEmptyEntries);
+            int.TryParse(s[0], out var date);
+            int.TryParse(s[1], out var monsdey);
+            int.TryParse(s[2], out var year);
+            DateTime dateTime = new DateTime(year, monsdey,date);            
+            return dateTime;
+                
+        }
+        public static int ToInt(this double b)
+        {
+            int a = (int)b;            
+            return a;
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Event_Extesion_Methods;
+using Google.Protobuf;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Action<string,int> action = (string b ,int a) => Console.WriteLine(b+", Тест № 1 ,"+a);
+action += (string a, int b) => Console.WriteLine(a+ ", Тест № 2 ," + b);
+action += (string a, int b) => Console.WriteLine(a + ", Тест № 3 ," + b);
+action("aa", 1);
+
+Func<int, double, string> func = (int a, double b) => (a + b).ToString();
+Console.WriteLine(func(5, 5)); 
+func += (int a, double b) => (a - b).ToString();
+Console.WriteLine(func(5, 5)); 
+func += (int a, double b) => (a * b).ToString();
+Console.WriteLine(func(5, 5)); 
+
+Predicate<int> predicate = (int a) => a == 5;
+Console.WriteLine(predicate(5));
+    predicate += (int b) => b > 5;
+Console.WriteLine(predicate(6));
+    predicate += (int c) => c < 5;
+Console.WriteLine(predicate(7));
+
+bool b = true;
+int i = b.ToInt();
+Console.WriteLine(i);
+
+string date = "12,11,2022";
+DateTime dateTime = date.ToDate();
+Console.WriteLine(dateTime.ToString());
+
+double d = 6.5;
+int g = d.ToInt();
+Console.WriteLine(g);
